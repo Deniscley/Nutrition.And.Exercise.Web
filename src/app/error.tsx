b5b9a60e1d09a99b8e975a486032331d00c575b6
Deniscley Marfran <deniscleymaf@gmail.com>
2023-08-10 11:09:41 -0300

    feat: Unused code removal and text translation.unused code removal and text translation.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,14 +16,14 @@ export default function Error({
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Algo deu errado!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
-        Try again
+        Tente novamente
       </button>
     </div>
   );
