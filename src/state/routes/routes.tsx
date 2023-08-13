2b1c8fbf63ae9b4b87a48d807a4fc78e7795e3de
Deniscley Marfran <deniscleymaf@gmail.com>
2023-08-13 20:40:36 -0300

    feat: Active nav creation

--- src/state/routes/routes.tsx
+++ src/state/routes/routes.tsx
@@ -1,25 +1,50 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Routes() {
+  const isActive = usePathname();
+  const activeNav = "active-nav";
+
   return (
     <ul className="nav__links">
       <li>
-        <Link href="/">Página inicial</Link>
+        <Link href="/" className={isActive === "/" ? activeNav : ""}>
+          Página inicial
+        </Link>
       </li>
       <li>
-        <Link href="/about">Sobre</Link>
+        <Link href="/about" className={isActive === "/about" ? activeNav : ""}>
+          Sobre
+        </Link>
       </li>
       <li>
-        <Link href="/gallery">Galeria</Link>
+        <Link
+          href="/gallery"
+          className={isActive === "/gallery" ? activeNav : ""}
+        >
+          Galeria
+        </Link>
       </li>
       <li>
-        <Link href="/plans">Planos</Link>
+        <Link href="/plans" className={isActive === "/plans" ? activeNav : ""}>
+          Planos
+        </Link>
       </li>
       <li>
-        <Link href="/contact">Contato</Link>
+        <Link
+          href="/trainers"
+          className={isActive === "/trainers" ? activeNav : ""}
+        >
+          Treinos
+        </Link>
       </li>
       <li>
-        <Link href="/trainers">Treinos</Link>
+        <Link
+          href="/contact"
+          className={isActive === "/contact" ? activeNav : ""}
+        >
+          Contato
+        </Link>
       </li>
     </ul>
   );
