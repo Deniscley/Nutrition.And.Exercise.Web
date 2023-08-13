4933770864682613078c16c29604e27f4b147305
Deniscley Marfran <deniscleymaf@gmail.com>
2023-08-13 14:43:28 -0300

    feat: Folder reorganization and route creation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "./globals.css";
+import "../styles/globals.css";
 import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import Navbar from "@/components/layout/navbar";
